4c4235fb63b83ca70a9bb0713665408943e97f0d
Fern <fmenezes098@gmail.com>
2025-10-23 11:09:26 -0300

    :sparkles: Criação da página

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -8,6 +8,21 @@ function Contato() {
   return (
     <>
       <Header />
+
+      <section className='contato-page'>
+        <h1>Contato</h1>
+        <p>Estamos sempre prontos para conversar! Seja para saber mais sobre nossos pets disponíveis para adoção, agendar uma visita, tirar dúvidas ou oferecer seu apoio, nossa equipe está à disposição para ajudar.</p>
+
+        <div className="contato-info">
+          <div className="dados-contato"></div>
+          <div className="form-contato">
+            <h3>Envie uma mensagem</h3>
+            <label htmlFor="nome">Nome</label>
+            <input type="text" name="nome" id="nome" />
+          </div>
+        </div>
+
+      </section>
       
 
       
